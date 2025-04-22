d3623ce932729e9b7bfa6fc3668a301e87c0b7c0
Antonino-Sofia <antoninosofia420@gmail.com>
2025-04-22 15:40:20 +0200

    js-jsnacks-blocco-5/snack1

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,14 @@
-const names = ['Edoardo', 'Simone', 'Francesco'];
+const names = ["Edoardo", "Simone", "Francesco"];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+//FOR
+for (let i = 0; i < names.length; i++) {
+  console, log(names[i]);
+}
+
+//FOREACH
+names.forEach((name) => {
+  console.log(name);
+});
